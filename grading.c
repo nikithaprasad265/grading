f326e043e3d2d4dd4023fbd4a8fbe7aae4219361
nikithaprasad265 <84224802+nikithaprasad265@users.noreply.github.com>
2021-05-23 21:01:11 +0530

    Add files via upload

---
+++ grading.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int m;
+	printf("Enter the marks");
+	scanf("%d",&m);
+	
+	if (m>85 && m<100)
+	 printf("Grade A");
+	else if (m>70 && m<84)
+	 printf("Grade B");
+	else if(m>55 && m<69)
+	 printf("Grade C");
+	else if(m>40 && m<54)
+	 printf("Grade D");
+	else if(m<40)
+	 printf("Grade F");
+	
+}
